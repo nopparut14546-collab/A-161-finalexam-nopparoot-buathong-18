743b4ed8227b9922f86832728ebf69a4a4bd5b05
nopparut14546-collab <nopparut.14546@gmail.com>
2025-12-02 10:21:58 +0700

    Buff Props
    -bool Immune not working yet

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,11 +2,32 @@ using UnityEngine;
 
 public class _Script : MonoBehaviour
 {
+    [SerializeField] public bool IsInvulnerable;
+
+    [SerializeField] public int MoveSpeed;
+
     //Public Fields
     private float moveSpeed = 5f;
     private float jumpForce = 10f;
 
     private Rigidbody2D rb;
+
+    public void SetMoveSpeed(float newSpeed)
+    {
+        newSpeed = MoveSpeed;
+    }
+
+    public void SetInvulnerability(bool IsInvulnerable)
+    {
+        if (IsInvulnerable == true)
+        {
+            Debug.Log("Player is Immune to All Damage.");
+        }
+        else
+        {
+            Debug.Log("Player is NOT immune.");
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +39,7 @@ public class _Script : MonoBehaviour
     void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput * MoveSpeed, rb.linearVelocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
